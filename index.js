9e327ad12209a52096b62f37388d85b86a3e3801
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:27:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ function nestedTarget() {
 };
 
 function inscreaseRankBy(n) {
- const rankedLists = document.querySelectorAll('.ranked-list');
- let children = rankedLists[i].children
-  for (let i = 0; i < rankedLists.length; i++) {
-    children[i].innerHTML = parseInt(children[i].innerHTML) + n;
-  }
-  for (let i = 0; i < children.length; i++) {
-    children[i].innerHTML = parseInt(children[i].innerHTML) + n;
-  }
+ const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+ }
 };
 
 function deepestChild() {
