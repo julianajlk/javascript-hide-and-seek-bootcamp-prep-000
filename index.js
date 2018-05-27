cea49379703fc6955e4858b4b72b53e96bf4bf11
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ function nestedTarget() {
 
 function inscreaseRankBy(n) {
  const rankedLists = document.querySelectorAll('.ranked-list');
- const firstList = rankedLists[0]
- const secondList = rankedLists[1]
  let children = rankedLists[i].children
- let secondChildren = secondList.children
   for (let i = 0; i < children.length; i++) {
     children[i].innerHTML = parseInt(children[i].innerHTML) + n;
   }
