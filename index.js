9995cfcdb7c10a7cac009f3b7c3c623916fb421e
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:13:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function inscreaseRankBy(n) {
  const rankedLists = document.querySelectorAll('.ranked-list');
  const firstList = rankedLists[0]
  const secondList = rankedLists[1]
- let children = firstList.children
+ let children = rankedLists[i].children
  let secondChildren = secondList.children
   for (let i = 0; i < children.length; i++) {
     children[i].innerHTML = parseInt(children[i].innerHTML) + n;
