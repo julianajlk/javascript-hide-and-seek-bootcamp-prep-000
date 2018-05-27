ae2228d182f7a89a273fa2ef4844bb0b12847f81
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:28:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 };
 
 function inscreaseRankBy(n) {
- const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ const rankedLists = document.querySelectorAll('ul.ranked-list li');
  for (let i = 0; i < lis.length; i++) {
    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
  }
