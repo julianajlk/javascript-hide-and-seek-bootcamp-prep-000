9e9abfb73f5f446b82bd3ef14dc4238762c546a8
Juliana Kang <julianajlk@gmail.com>
2018-05-27 22:48:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  const elements = document.getElementById(nested);
+  return elements.querySelector(.target);
+}
+
+function inscreaseRankBy(n) {
+
+};
