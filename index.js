58cc9da2cf31a0633762f74be4c5f3786a6dd09e
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:44:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+};
+
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list');
+  const firstList = lists[0];
+  const secondList = lists[1];
+  let firstListChildren = firstList.children;
+  let secondListChildren = secondList.children;
+    for (let i = 0; i < firstListChildren.length; i++) {
+      firstListChildren[i].innerHTML = parseInt(firstListChildren[i].innerHTML) + n;
+      }
+    for (let i = 0; i < secondListChildren.length; i++) {
+      secondListChildren[i].innerHTML = parseInt(secondListChildren[i].innerHTML) + n;
+      }
+};
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node');
+  return grandNode.querySelector('div div div div div')
+};
