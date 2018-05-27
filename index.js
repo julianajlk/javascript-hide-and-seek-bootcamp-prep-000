82f196ef9af383bcf38bce66538e22024b5a8504
Juliana Kang <julianajlk@gmail.com>
2018-05-27 23:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function inscreaseRankBy(n) {
   for (let i = 0; i < secondChildren.length; i++) {
     secondChildren[i].innerHTML = parseInt(secondChildren[i].innerHTML) + n;
   }
-  return rankedLists
+  return null;
 };
 
 function deepestChild() {
